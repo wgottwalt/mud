2e3768c5f1efccb8ef0b59dce0ddb1abe06fc63d
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-01-24 15:04:19 +0100

    changed Log component defaults
    Changed string to c++ strings, changed casting functions to templates
    and made use of new concepts. Changed makefile because Log defaults are
    now template/constants only.

--- sources/Log/Defaults.hxx
+++ sources/Log/Defaults.hxx
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <string>
+#include "Support/Time.hxx"
 
 namespace Mud::Log
 {
-    static const char DefaultPath[] = "/tmp/mud";
-    static const char DefaultInfo[] = "mud_info.log";
-    static const char DefaultWarn[] = "mud_warn.log";
-    static const char DefaultError[] = "mud_error.log";
-    static const char DefaultFatal[] = "mud_fatal.log";
-    static const char DefaultDebug[] = "mud_debug.log";
+    static const std::string DefaultPath("/tmp/mud");
+    static const std::string DefaultInfo("mud_info.log");
+    static const std::string DefaultWarn("mud_warn.log");
+    static const std::string DefaultError("mud_error.log");
+    static const std::string DefaultFatal("mud_fatal.log");
+    static const std::string DefaultDebug("mud_debug.log");
 
-    enum class Type : int8_t {
+    enum class Level {
         Info,
         Warn,
         Error,
@@ -19,6 +19,51 @@ namespace Mud::Log
         Debug
     };
 
-    std::string logToString(const Type type);
-    Type stringToLog(const std::string &str);
+    struct Entry {
+        const Level level;
+        const Support::Time::Timepoint stamp;
+        const std::string component;
+        const std::string message;
+    };
+
+    template <Support::Concepts::Enum T>
+    constexpr std::string levelToString(const T level) noexcept
+    {
+        switch (level)
+        {
+            case Level::Info:
+                return "info";
+
+            case Level::Warn:
+                return "warn";
+
+            case Level::Error:
+                return "error";
+
+            case Level::Fatal:
+                return "fatal";
+
+            case Level::Debug:
+            default:
+                return "debug";
+        }
+    }
+
+    template <Support::Concepts::Enum T>
+    constexpr T stringToLevel(const std::string &str) noexcept
+    {
+        if (str == "info")
+            return Level::Info;
+
+        if (str == "warn")
+            return Level::Warn;
+
+        if (str == "error")
+            return Level::Error;
+
+        if (str == "fatal")
+            return Level::Fatal;
+
+        return Level::Debug;
+    }
 }
