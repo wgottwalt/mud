ae944ce9018354226a89fa77ec60057bd2e319e8
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-01-24 08:10:37 +0100

    fixed log string constant names

--- sources/LogDefaults.hxx
+++ sources/LogDefaults.hxx
@@ -4,12 +4,12 @@
 
 namespace Mud::Log
 {
-    static const char LogPath[] = "/tmp/mud";
-    static const char LogInfo[] = "mud_info.log";
-    static const char LogWarn[] = "mud_warn.log";
-    static const char LogError[] = "mud_error.log";
-    static const char LogFatal[] = "mud_fatal.log";
-    static const char LogDebug[] = "mud_debug.log";
+    static const char DefaultPath[] = "/tmp/mud";
+    static const char DefaultInfo[] = "mud_info.log";
+    static const char DefaultWarn[] = "mud_warn.log";
+    static const char DefaultError[] = "mud_error.log";
+    static const char DefaultFatal[] = "mud_fatal.log";
+    static const char DefaultDebug[] = "mud_debug.log";
 
     enum class Type : int8_t {
         Info,
