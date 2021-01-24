d7de42eaee3dccb0dff33a8ab0110dc9c82a86b7
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-01-24 07:22:12 +0100

    added stringify string support template

--- sources/UtilString.hxx
+++ sources/UtilString.hxx
@@ -1,9 +1,23 @@
 #pragma once
 
+#include <sstream>
 #include <string>
 
 namespace Mud::Util
 {
+    template <typename H, typename... T>
+    std::string str(const H &head, const T &...tail)
+    {
+        std::stringstream strm;
+
+        if constexpr (sizeof... (T))
+            strm << head << str(tail...);
+        else
+            strm << head;
+
+        return strm.str();
+    }
+
     std::string trim(const std::string &data, const char delim, const bool at_start = true,
                      const bool at_end = true);
     std::string trim(const std::string &data, const bool at_start = true, const bool at_end = true);
