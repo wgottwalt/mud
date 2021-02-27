8813da08dc016f81ef5b1523ddac4c84f1a2746a
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-02-27 16:51:58 +0100

    updated support string function
    Replaced all string support functions by templates, which makes less
    trouble for now.

--- sources/Support/String.hxx
+++ sources/Support/String.hxx
@@ -5,6 +5,35 @@
 
 namespace Mud::Support::String
 {
+    namespace Impl
+    {
+        template <bool Begin, bool End>
+        std::string __trim(std::string &&str, const char delim)
+        {
+            if constexpr (Begin)
+                while (!str.empty() && (*(str.begin())) == delim)
+                    str.erase(str.begin(), str.begin() + 1);
+            if constexpr (End)
+                while (!str.empty() && (*(str.end() - 1)) == delim)
+                    str.erase(str.end() - 1, str.end());
+
+            return str;
+        }
+
+        template <bool Begin, bool End>
+        std::string __trim(std::string &&str)
+        {
+            if constexpr (Begin)
+                while (!str.empty() && std::isspace(*(str.begin())))
+                    str.erase(str.begin(), str.begin() + 1);
+            if constexpr (End)
+                while (!str.empty() && std::isspace(*(str.end() - 1)))
+                    str.erase(str.end() - 1, str.end());
+
+            return str;
+        }
+    }
+
     template <typename H, typename... T>
     std::string str(const H &head, const T &...tail)
     {
@@ -18,14 +47,43 @@ namespace Mud::Support::String
         return strm.str();
     }
 
-    void trim(std::string &str, const char delim, const bool at_start = true,
-              const bool at_end = true);
-    std::string trim(const std::string &str, const char delim, const bool at_start = true,
-                     const bool at_end = true);
-    std::string trim(std::string &&str, const char delim, const bool at_start = true,
-                     const bool at_end = true);
+    template <bool Begin = true, bool End = true>
+    void trim(std::string &str, const char delim)
+    {
+        Impl::__trim<Begin,End>(std::move(str), delim);
+    }
 
-    void trim(std::string &str, const bool at_start = true, const bool at_end = true);
-    std::string trim(const std::string &str, const bool at_start = true, const bool at_end = true);
-    std::string trim(std::string &&str, const bool at_start = true, const bool at_end = true);
+    template <bool Begin = true, bool End = true>
+    std::string trim(const std::string &str, const char delim)
+    {
+        std::string tmp(str);
+
+        return Impl::__trim<Begin,End>(std::move(tmp), delim);
+    }
+
+    template <bool Begin = true, bool End = true>
+    std::string trim(std::string &&str, const char delim)
+    {
+        return Impl::__trim<Begin,End>(std::move(str), delim);
+    }
+
+    template <bool Begin = true, bool End = true>
+    void trim(std::string &str)
+    {
+        Impl::__trim<Begin,End>(std::move(str));
+    }
+
+    template <bool Begin = true, bool End = true>
+    std::string trim(const std::string &str)
+    {
+        std::string tmp(str);
+
+        return Impl::__trim<Begin,End>(std::move(tmp));
+    }
+
+    template <bool Begin = true, bool End = true>
+    std::string trim(std::string &&str)
+    {
+        return Impl::__trim<Begin,End>(std::move(str));
+    }
 }
